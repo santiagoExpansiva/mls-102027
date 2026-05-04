1f45bcf6dea1f787ee5594a870e6ea44d3b7b020
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-05-04 09:36:44 -0300

    Update customElement.test.ts

--- l2/customElement.test.ts
+++ l2/customElement.test.ts
@@ -1,5 +1,1 @@
 /// <mls fileReference="_102027_/l2/customElement.test.ts" enhancement="_blank" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
