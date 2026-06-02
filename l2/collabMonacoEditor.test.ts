639a339a05077e011973a916737f76afa9fe9b65
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-06-02 17:58:04 +0000

    group-1
    group-1

---
+++ l2/collabMonacoEditor.test.ts
@@ -0,0 +1,2 @@
+/// <mls fileReference="_102027_/l2/collabMonacoEditor.test.ts" enhancement="_blank"/>
+
---
+++ l2/collabMonacoEditor.ts
@@ -0,0 +1,32 @@
+/// <mls fileReference="_102027_/l2/collabMonacoEditor.ts" enhancement="_102027_/l2/enhancementLit.ts"/>
+
+import { nothing } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { StateLitElement } from '/_102027_/l2/stateLitElement.js';
+
+@customElement('collab-monaco-editor-102027')
+export class CollabMonacoEditor extends StateLitElement {
+
+    @property({ attribute: 'msize' }) msize: string = '';
+
+    public mlsEditor: any;
+
+    get isMls2(): boolean {
+        return !!this.closest('collab-page');
+    }
+
+    render() { return nothing; }
+
+    updated(changed: Map<PropertyKey, unknown>) {
+        super.updated(changed);
+        if (changed.has('msize')) this._onMsizeChange(this.msize);
+    }
+
+    private _onMsizeChange(msize: string) {
+        const [width, height] = msize.split(',');
+        if (!width || !height) return;
+        if (this.mlsEditor && typeof this.mlsEditor.layout === 'function') {
+            this.mlsEditor.layout({ width: +width, height: +height });
+        }
+    }
+}
